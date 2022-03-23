36d3e84786628077cdd7e69e9696a5f4ea6148f3
usamabasra1122 <usamabasra1122@gmail.com>
2022-03-23 01:37:17 +0000

    hk

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tv6n6r^+8llw3z%-vpf&lruos**t()ovh@rj)^_-rn&m4#fcf4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
